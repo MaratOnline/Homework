05124189045c3b52de8d6fb3ef67aeb749a0ea5f
MaratOnline <marat.online@live.ru>
2013-12-17 07:30:35 +0300

    HW1
    Задача №1

--- HW1 PR/src/Main.java
+++ HW1 PR/src/Main.java
@@ -1,31 +1,37 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Марат
- * Date: 23.09.13
- * Time: 10:53
- * To change this template use File | Settings | File Templates.
- */
 import java.util.regex.*;
 
+/**
+ * Created by Марат on 14.12.13.
+ */
 public class Main {
+    static String numberPhone = null;
     private static void example(String number){
         Pattern pattern;
-        pattern = Pattern.compile("\\+([0-9]{1,3})\\(([0-9]{2,8})\\)\\d([0-9]{1,6})\\-\\d([0-9]{1,6})\\-\\d([0-9]{1,6})");
+        pattern = Pattern.compile("\\+\\d{1,3}\\(\\d{2,8}\\)\\d{1,6}\\-\\d{1,6}\\-\\d{1,6}");
         Matcher matcher = pattern.matcher(number);
 
-        System.out.print("Find: ");
-        // Поиск соответствия шаблону в строку (поиск подстроки)
         if(matcher.find()){
-            System.out.println("matches");
-            System.out.println("Задание №1: " + matcher.group()); // Выводим всю строку
+            numberPhone = matcher.group();
+            System.out.println("Задание №1: " + numberPhone); // Выводим всю строку
         }else{
-            System.out.println("Not matched");
+            System.out.println("Задание №1: Номер не прошел проверку!");
         }
+    }
+    private static void standart(String number) {
+        example(number);
+        if (numberPhone!=null) {
+            System.out.println("Задание №2: " + numberPhone.replaceAll("\\(", "").replaceAll("\\)","").replaceAll("-", ""));
+            numberPhone=null;
+        } else {
+            System.out.println("Задание №2: Номер не прошел проверку в первом задании!");
+        }
+        System.out.println("*****************************");
     }
 
     public static void main(String [] args) {
-        example("+7(969)622-84-49");   // подходящий номер
-        example("+3(475)69-664-65");   // подходящий номер
-        example("+3(475233213)69-664-65");     //не подходящий номер
+        standart("+7(969)622-84-49");   // подходящий номер
+        standart("+3223(475)69-664-65"); // не подходящий номер
+        standart("+3(475)69-664-65");   // подходящий номер
+        standart("+3(475233213)69-664-65"); //не подходящий номер
     }
 }
